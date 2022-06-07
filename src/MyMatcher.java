8419ee85d3c6583d546fd28621321cd06f216965
maevskiEd <maevski.ed@gmail.com>
2022-06-07 15:30:08 +0300

    test3

---
+++ src/MyMatcher.java
@@ -0,0 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MyMatcher {
+    public static void main(String[] args) {
+        String text = "У Сени и Сани в сенях сом с усами";
+//        Pattern pattern = Pattern.compile("\\b.+\\b");
+        Pattern pattern = Pattern.compile("\\b[Сс][\\dа-яА-Я_]*\\b");
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(text.substring(matcher.start(), matcher.end()));
+        }
+    }
+}
