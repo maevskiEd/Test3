0091b8cd2de66ee64ffc9827f6f6f945775e26f1
maevskiEd <maevski.ed@gmail.com>
2022-06-07 15:36:30 +0300

    test3 new commit2

--- src/MyMatcher.java
+++ src/MyMatcher.java
@@ -5,6 +5,7 @@ public class MyMatcher {
     public static void main(String[] args) {
         String text = "У Сени и Сани в сенях сом с усами";
         String text2 = "У Сени и Сани в сенях сом с усами";
+        String text3 = "У Сени и Сани в сенях сом с усами";
 //        Pattern pattern = Pattern.compile("\\b.+\\b");
         Pattern pattern = Pattern.compile("\\b[Сс][\\dа-яА-Я_]*\\b");
         Matcher matcher = pattern.matcher(text);
